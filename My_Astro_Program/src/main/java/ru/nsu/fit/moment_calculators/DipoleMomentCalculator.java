48468cb3fe4532ce8027fbe5aa7eb87b728749da
Olga <o.pogibelnaya@g.nsu.ru>
2024-12-17 22:14:06 +0700

    добавление класса DipoleMomentCalculator и его примитивного теста

---
+++ My_Astro_Program/src/main/java/ru/nsu/fit/moment_calculators/DipoleMomentCalculator.java
@@ -0,0 +1,53 @@
+package ru.nsu.fit.moment_calculators;
+
+public class DipoleMomentCalculator extends MomentCalculator {
+
+    public DipoleMomentCalculator(double[][] matrix, boolean[][] mask) {
+        super(matrix, mask);
+    }
+
+    private void calcSums(int x, int y, int x1, int y1, int centerY, double[] sums) {
+        // sums[0] = sumX, sums[1] = sumY, sums[2] = sumX1, sums[3] = sumY1
+        sums[0] = matrix[y][x]; // sumX += яркость (y; x)
+        sums[1] = matrix[y][x]; // sumY += яркость (y; x)
+        sums[2] = 0;
+        sums[3] = 0;
+        if (x1 != x) {
+            sums[2] += matrix[y][x1]; // sumX1 += яркость (y; x1)
+            sums[1] += matrix[y][x1]; // sumY += яркость (y; x1)
+        }
+        if (y1 != y) {
+            sums[0] += matrix[y1][x]; // sumX += яркость (y1; x)
+            sums[3] += matrix[y1][x]; // sumY1 += яркость (y1; x)
+            if (x1 != x) {
+                sums[2] += matrix[y1][x1]; // sumX1 += яркость (y1; x1)
+                sums[3] += matrix[y1][x1]; // sumY1 += яркость (y1; x1)
+            }
+            sums[3] *= (y1 - centerY);
+        }
+    }
+
+    @Override
+    public double[] calculate(int centerX, int centerY, int startX, int endX, int startY, int endY) {
+        double[] pXpY = new double[]{0, 0};
+        int doubleCenterX = 2 * centerX;
+        int doubleCenterY = 2 * centerY;
+        double[] sums = new double[4];  // sums[0] = sumX, sums[1] = sumY, sums[2] = sumX1, sums[3] = sumY1
+        for (int x = startX; x <= endX; x++) {
+            int offsetX = x - startX;
+            int x1 = doubleCenterX - x;
+            double distanceX = (double) x - centerX;
+            double distanceX1 = (double) x1 - centerX;
+            for (int y = startY; y <= endY; y++) {
+                if (!mask[offsetX][y - startY]) continue;
+                int y1 = doubleCenterY - y;
+                double distanceY = (double) y - centerY;
+                calcSums(x, y, x1, y1, centerY, sums);
+                pXpY[0] += distanceX * sums[0] + distanceX1 * sums[2];
+                pXpY[1] += distanceY * sums[1] + sums[3];
+            }
+        }
+        return pXpY;
+    }
+
+}
---
+++ My_Astro_Program/src/test/java/ru/nsu/fit/moment_calculators/DipoleMomentCalculatorTest.java
@@ -0,0 +1,44 @@
+package ru.nsu.fit.moment_calculators;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class DipoleMomentCalculatorTest {
+    private final double[][] matrix = {
+            {0, 0, 1, 0, 0, 0, 1, 1, 0},
+            {0, 2, 2, 2, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 2, 2, 2, 0, 0, 0, 0, 0},
+            {1, 0, 1, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0}
+    };
+    private final boolean[][] mask = {
+            {false, false, true},
+            {false, true, true},
+            {true, true, true}
+    };
+    private final int radius = 2;
+    private final DipoleMomentCalculator dipoleMomentCalculator = new DipoleMomentCalculator(matrix, mask);
+
+    @Test
+    void test1() {
+        double[] moment = dipoleMomentCalculator.calculate(radius, radius, 0, radius, 0, radius);
+        Assertions.assertEquals(0.0, moment[0]);
+        Assertions.assertEquals(0.0, moment[1]);
+        Assertions.assertEquals(0.0, Math.hypot(moment[0], moment[1]));
+    }
+
+    @Test
+    void test2() {
+        int centerX = 3;
+        int centerY = 4;
+        double[] moment = dipoleMomentCalculator.calculate(centerX, centerY, centerX - radius, centerX, centerY - radius, centerY);
+        Assertions.assertEquals(-3.0, moment[0]);
+        Assertions.assertEquals(-4.0, moment[1]);
+        Assertions.assertEquals(5.0, Math.hypot(moment[0], moment[1]));
+
+    }
+}
